fe6cdc16efc45ecf9f5bc2c3eb85cef4a79ba61c
Hyungon_Ryu <yhgon@users.noreply.github.com>
2020-10-11 16:25:19 +0900

    Delete __init__.py

--- compressive_transformer_pytorch/__init__.py
+++
@@ -1,2 +0,0 @@
-from compressive_transformer_pytorch.compressive_transformer_pytorch import CompressiveTransformer
-from compressive_transformer_pytorch.autoregressive_wrapper import AutoregressiveWrapper
